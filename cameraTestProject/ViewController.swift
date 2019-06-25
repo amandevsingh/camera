bc645b1981f89939445458e31ae95788834f4b7f
Aman dev <aman.dev@mac55f.local>
2019-06-26 00:11:26 +0530

    Initial Commit

---
+++ cameraTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cameraTestProject
+//
+//  Created by Aman dev on 6/26/19.
+//  Copyright © 2019 Aman dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
