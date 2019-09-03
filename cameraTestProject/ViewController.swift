d4f6f578b96f0a95e5d34f9dfba035975b7e17d4
Aman dev <aman.dev@MAC55F.local>
2019-09-04 02:42:32 +0530

    update

--- cameraTestProject/ViewController.swift
+++ cameraTestProject/ViewController.swift
@@ -7,17 +7,51 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
 
+    var session: AVCaptureSession?
+    var stillImageOutput: AVCaptureStillImageOutput?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    
+    @IBOutlet weak var photoPreviewImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        session = AVCaptureSession()
+        session!.sessionPreset = AVCaptureSession.Preset.photo
+        let backCamera =  AVCaptureDevice.default(for: AVMediaType.video)
+            var error: NSError?
+            var input: AVCaptureDeviceInput!
+            do {
+                input = try AVCaptureDeviceInput(device: backCamera!)
+            } catch let error1 as NSError {
+                error = error1
+                input = nil
+                print(error!.localizedDescription)
+            }
+            if error == nil && session!.canAddInput(input) {
+                session!.addInput(input)
+                stillImageOutput = AVCaptureStillImageOutput()
+                stillImageOutput?.outputSettings = [AVVideoCodecKey:  AVVideoCodecJPEG]
+                if session!.canAddOutput(stillImageOutput!) {
+                    session!.addOutput(stillImageOutput!)
+                    videoPreviewLayer = AVCaptureVideoPreviewLayer(session: session!)
+                    videoPreviewLayer!.videoGravity =    AVLayerVideoGravity.resizeAspect
+                    videoPreviewLayer!.connection?.videoOrientation =   AVCaptureVideoOrientation.portrait
+                    previewView.layer.addSublayer(videoPreviewLayer!)
+                    session!.startRunning()
+                }
+            }
+        }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        videoPreviewLayer!.frame = previewView.bounds
+    }
+  
+
+    @IBAction func didTapOnTakePhotoButton(_ sender: UIButton) {
     }
 
 
